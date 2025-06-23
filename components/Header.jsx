a915072b07fa4baf42374a62cf33c44fa3808e94
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-06-24 02:24:47 +0300

    up

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,23 @@
-'use client';
+"use client";
 import { useState } from "react";
 import Link from "next/link";
 import { HiOutlineViewList } from "react-icons/hi";
 import { usePathname } from "next/navigation";
-import classes from '../style/header.module.css'
+import classes from "../style/header.module.css";
+import Image from "next/image";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-const path = usePathname()
+  const [register, setRegister] = useState(false);
+  const toggle = () => {
+    setRegister(!register);
+  };
+  const path = usePathname();
   return (
-    <div className={`w-full max-w-[1280px] mx-auto mt-[24px] h-[56px] flex justify-between items-center relative px-4
+    <div
+      className={`w-full max-w-[1280px] mx-auto mt-[24px] h-[56px] flex justify-between items-center relative px-4
     
-    `}>
+    `}
+    >
       {/* Logo */}
       <div className="logo">
         <h1 className="font-bold font-worksans text-[48px] h-full">VENVL</h1>
@@ -18,18 +25,38 @@ const path = usePathname()
 
       {/* Desktop Navigation */}
       <nav className="nav w-full max-w-[590px] justify-between items-center hidden lg:flex">
-        <Link href="/" className={` font-semibold font-inter text-[18px]
-          ${path ==='/'?classes.nav_link:undefined}
-          `}>Home</Link>
-        <Link href="/about" className={` font-semibold font-inter text-[18px]
-          ${path ==='/about'?classes.nav_link:undefined}
-          `}>About</Link>
-        <Link href="/featured" className={` font-semibold font-inter text-[18px]
-          ${path ==='/featured'?classes.nav_link:undefined}
-          `}>Featured</Link>
-        <Link href="/contact" className={` font-semibold font-inter text-[18px]
-          ${path ==='/contact'?classes.nav_link:undefined}
-          `}>Contact Us</Link>
+        <Link
+          href="/"
+          className={` font-semibold font-inter text-[18px]
+          ${path === "/" ? classes.nav_link : undefined}
+          `}
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={` font-semibold font-inter text-[18px]
+          ${path === "/about" ? classes.nav_link : undefined}
+          `}
+        >
+          About
+        </Link>
+        <Link
+          href="/featured"
+          className={` font-semibold font-inter text-[18px]
+          ${path === "/featured" ? classes.nav_link : undefined}
+          `}
+        >
+          Featured
+        </Link>
+        <Link
+          href="/contact"
+          className={` font-semibold font-inter text-[18px]
+          ${path === "/contact" ? classes.nav_link : undefined}
+          `}
+        >
+          Contact Us
+        </Link>
       </nav>
 
       {/* Desktop Utilities */}
@@ -38,7 +65,29 @@ const path = usePathname()
           <option value="">Arabic</option>
           <option value="">English</option>
         </select>
-        <button className="w-[87px] h-[36px] bg-black text-white font-inter text-[14px] rounded-md">Register</button>
+        {/* -------------- */}
+
+        {!register && (
+          <button
+            className="w-[87px] h-[36px] bg-black text-white font-inter text-[14px] rounded-md"
+            onClick={toggle}
+          >
+            Register
+          </button>
+        )}
+        {/* ----switch------ */}
+        {register && (
+          <div
+            className={`w-[142px] h-[52px] bg-white rounded-4xl flex gap-[16px] px-2 items-center border-[1px]`}
+            onClick={toggle}
+          >
+            <div className="w-[36px] h-[36px] relative overflow-hidden rounded-full">
+              <Image src="/villa.png" alt="user" fill />
+            </div>
+            <span>Mahmoud</span>
+          </div>
+        )}
+        {/* -------------- */}
       </div>
 
       {/* Hamburger Icon - Mobile */}
@@ -61,16 +110,42 @@ const path = usePathname()
           ${isOpen ? "translate-x-0" : "-translate-x-[250px]"}`}
       >
         <div className="p-4 flex flex-col gap-4 mt-[60px]">
-          <Link href="/" className="font-semibold font-inter text-[18px]" onClick={() => setIsOpen(false)}>Home</Link>
-          <Link href="/about" className="font-semibold font-inter text-[18px]" onClick={() => setIsOpen(false)}>About</Link>
-          <Link href="/featured" className="font-semibold font-inter text-[18px]" onClick={() => setIsOpen(false)}>Featured</Link>
-          <Link href="/contact" className="font-semibold font-inter text-[18px]" onClick={() => setIsOpen(false)}>Contact Us</Link>
+          <Link
+            href="/"
+            className="font-semibold font-inter text-[18px]"
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="font-semibold font-inter text-[18px]"
+            onClick={() => setIsOpen(false)}
+          >
+            About
+          </Link>
+          <Link
+            href="/featured"
+            className="font-semibold font-inter text-[18px]"
+            onClick={() => setIsOpen(false)}
+          >
+            Featured
+          </Link>
+          <Link
+            href="/contact"
+            className="font-semibold font-inter text-[18px]"
+            onClick={() => setIsOpen(false)}
+          >
+            Contact Us
+          </Link>
 
           <select>
             <option value="">Arabic</option>
             <option value="">English</option>
           </select>
-          <button className="w-[87px] h-[36px] bg-black text-white font-inter text-[14px] rounded-md">Register</button>
+          <button className="w-[87px] h-[36px] bg-black text-white font-inter text-[14px] rounded-md">
+            Register
+          </button>
         </div>
       </div>
     </div>
